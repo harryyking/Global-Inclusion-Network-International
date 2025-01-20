20556aa8f47e632e7a20ac2f0771525454e581c0
harryyking <arthurharry06@gmail.com>
2025-01-20 21:27:17 +0000

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutUs from "@/components/AboutUs";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import MissionVision from "@/components/MissionVision";
@@ -13,6 +14,7 @@ export default function Home() {
       <AboutUs/>
       <MissionVision/>
       <Testimonials/>
+      <Footer/>
     </div>
   );
 }
