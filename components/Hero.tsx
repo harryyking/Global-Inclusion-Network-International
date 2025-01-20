116a9ac3e51f896ef55c25a7732b77ceb8734930
harryyking <arthurharry06@gmail.com>
2025-01-20 21:53:35 +0000

    new hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,25 @@
 export default function Hero() {
   return (
-    <section className="hero bg-primary text-primary-content py-24">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold mb-6">Empowering Every Child, Regardless of Ability</h1>
-          <p className="text-xl mb-10">
-            Global Inclusion Network International is dedicated to creating inclusive communities and unlocking the
-            potential of children with disabilities.
-          </p>
-          <button className="btn btn-secondary btn-lg">Learn More</button>
-        </div>
+    <section
+    className="hero min-h-screen"
+    style={{
+      backgroundImage: "url(https://images.pexels.com/photos/9968379/pexels-photo-9968379.jpeg?auto=compress&cs=tinysrgb&w=600)",
+    }}>
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-neutral-content">
+      <div className="max-w-6xl">
+        <h1 className="mb-5 text-5xl font-bold">Empowering Every Child, Regardless of Ability</h1>
+        <p className="mb-5">
+        Global Inclusion Network International is dedicated to creating inclusive communities and unlocking the
+        potential of children with disabilities.
+        </p>
+        <button className="btn btn-primary">Get Started</button>
       </div>
-    </section>
+    </div>
+  </section>
   )
 }
+
+
+
 
