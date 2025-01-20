46fea78f6b3c64804daabe08726fd7b7961eda2f
harryyking <arthurharry06@gmail.com>
2025-01-20 21:13:34 +0000

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {Open_Sans} from 'next/font/google'
 
-const brand = Open_Sans({weight:["300","400","600","700","800"],})
+const brand = Open_Sans({weight:["300","400","600","700","800"], subsets:['latin']})
 
 export const metadata: Metadata = {
   title: "Create Next App",
