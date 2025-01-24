a8f69bf55f7a03e361b819217f51d94d822e5598
harryyking <arthurharry06@gmail.com>
2025-01-24 10:59:10 +0000

    cta edit

--- components/Volunteer.tsx
+++ components/Volunteer.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Volunteer = () => {
   return (
-    <div className='my-4'>
+    <div className='my-4 max-w-7xl mx-auto'>
 
     <div
-      className="relative h-[350px] w-full bg-cover bg-center flex items-center justify-center max-w-7xl mx-auto px-4"
+      className="relative h-[350px] w-full bg-cover bg-center flex items-center justify-center"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/9968380/pexels-photo-9968380.jpeg?auto=compress&cs=tinysrgb&w=600')`,
       }}
@@ -22,7 +22,7 @@ const Volunteer = () => {
         <Link
           href="https://docs.google.com/forms/d/e/1FAIpQLSe-mSta2f397t5iKFULWo2QHn3YREVwTertBeAtnWZYtAeD1A/viewform" // Replace with your actual route
           target="_blank"
-          className="btn btn-secondary btn-md"
+          className="btn btn-secondary btn-md text-white"
         >
           Join Us
         </Link>
